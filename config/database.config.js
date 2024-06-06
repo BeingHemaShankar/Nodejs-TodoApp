dc0fd2c67be75a9cbe887989239be3895ff0acc1
BeingHemaShankar <hemashankar005@gmail.com>
2024-06-06 09:00:58 +0530

    Initial commit
    Initial commit

--- config/database.config.js
+++ config/database.config.js
@@ -1,4 +1,3 @@
 module.exports = {
     // url: 'mongodb://localhost:27017/crud-node-express'
-    url :'mongodb+srv://hemashankar005:admin@cluster0.zmrwnzl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 }
